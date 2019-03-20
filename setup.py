5751dff11b72ee167e8f039f4f6dd03a9cf887ca
Hagen Wierstorf <hwierstorf@audeering.com>
2019-03-20 10:19:18 +0100

    Add audiofile package

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from io import open
+
+
+def read_file(file_name):
+    with open(file_name, encoding="utf-8") as stream:
+        return stream.read().split("\n")[:-1]
+
+
+readme = read_file('README.rst')
+description = readme[1]
+long_description = '\n'.join(readme)
+
+setup(
+    name='audiofile',
+    packages=find_packages(),
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'numpy',
+        'soundfile',
+        'sox',
+    ],
+    author='Hagen Wierstorf',
+    author_email='hwierstorf@audeering.com',
+    description=description,
+    long_description_content_type='text/x-rst',
+    license='MIT',
+    keywords=['audio tools'],
+    url='',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+    ],
+)
