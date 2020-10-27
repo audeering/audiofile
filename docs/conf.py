34ebb73bd21c79354d4a943f800b693fb3af7956
Hagen Wierstorf <hwierstorf@audeering.com>
2020-10-27 19:28:41 +0100

    DOC: fix copy button bash prompt removal

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ intersphinx_mapping = {
 # Ignore package dependencies during building the docs
 autodoc_mock_imports = ['soundfile']
 
-copybutton_prompt_text = r'>>> |\.\.\. |$ '
+copybutton_prompt_text = r'>>> |\.\.\. |\$ '
 copybutton_prompt_is_regexp = True
 
 # HTML --------------------------------------------------------------------
