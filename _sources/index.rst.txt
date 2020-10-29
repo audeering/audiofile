8c4fb7c06d418536cd9d345b928bb9d773e232aa
hagenw <hagenw@users.noreply.github.com>
2020-10-29 09:31:59 +0000

    deploy: 8b5a70f95bbc65778d386be8a4595239d35f563d

--- _sources/index.rst.txt
+++ _sources/index.rst.txt
@@ -6,6 +6,7 @@
     
     installation
     usage
+    benchmark
     contributing
     changelog
 
--- _static/copybutton.js
+++ _static/copybutton.js
@@ -133,7 +133,7 @@ function formatCopyText(textContent, copybuttonPromptText, isRegexp = false, onl
 
 var copyTargetText = (trigger) => {
   var target = document.querySelector(trigger.attributes['data-clipboard-target'].value);
-  return formatCopyText(target.innerText, '', false, true,  true)
+  return formatCopyText(target.innerText, '>>> |\\.\\.\\. |\\$ ', true, true,  true)
 }
 
   // Initialize with a callback so we can modify the text before copy
