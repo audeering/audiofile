b47aca59006181826ff76a370887d7125b724ec9
Hagen Wierstorf <hwierstorf@audeering.com>
2021-08-05 09:54:04 +0200

    Ensure paths are preserved under run() in Windows

--- audiofile/core/utils.py
+++ audiofile/core/utils.py
@@ -14,8 +14,14 @@ def file_extension(path):
 
 def run(shell_command):
     """Return the output of a shell command provided as string."""
+    # posix=False ensure paths with \\ are preserved under Winodws
+    # see https://stackoverflow.com/a/63534016
+    if sys.platform == 'win32':
+        posix = False
+    else:
+        posix = True
     out = subprocess.check_output(
-        shlex.split(shell_command),
+        shlex.split(shell_command, posix=posix),
         stderr=subprocess.STDOUT
     )
     try:
