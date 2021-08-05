614fbad949610eb900ad8643f14a0ba4f6647a0f
Hagen Wierstorf <hwierstorf@audeering.com>
2021-08-05 12:37:37 +0200

    Reenable MP3 test

--- tests/test_audiofile.py
+++ tests/test_audiofile.py
@@ -220,9 +220,9 @@ def test_file_type(tmpdir, file_type, magnitude, sampling_rate, channels):
 @pytest.mark.parametrize('magnitude', [0.01])
 def test_mp3(tmpdir, magnitude, sampling_rate, channels):
 
-    # Currently we are not able to setup the Windows runner with MP3 support
-    if sys.platform == 'win32':
-        return
+    # # Currently we are not able to setup the Windows runner with MP3 support
+    # if sys.platform == 'win32':
+    #     return
 
     signal = sine(magnitude=magnitude,
                   sampling_rate=sampling_rate,
@@ -270,10 +270,6 @@ def test_mp3(tmpdir, magnitude, sampling_rate, channels):
 
 def test_formats():
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    # script_dir = 'tests'
-    print('DEBUG: ', audeer.list_dir_names(script_dir))
-    print('DEBUG: ', audeer.list_file_names(script_dir))
-    print('DEBUG: ', af.core.utils.run('mediainfo --Version'))
     assests_dir = os.path.join(script_dir, 'assets')
     files = [
         'gs-16b-1c-44100hz.opus',
