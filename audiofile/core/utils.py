91f4b8d491584e119081dd30377a4422acf417fe
Hagen Wierstorf <hwierstorf@audeering.com>
2021-08-05 10:18:52 +0200

    Preserve / in file path

--- audiofile/core/utils.py
+++ audiofile/core/utils.py
@@ -17,8 +17,10 @@ def run(shell_command):
     """Return the output of a shell command provided as string."""
     # posix=False ensure paths with \\ are preserved under Winodws
     # see https://stackoverflow.com/a/63534016
+    command = shlex.split(shell_command, posix=(sys.platform != 'win32'))[:1]
+    args = shlex.split(shell_command)[1:]
     out = subprocess.check_output(
-        shlex.split(shell_command, posix=(sys.platform != 'win32')),
+        command + args,
         stderr=subprocess.STDOUT
     )
     try:
