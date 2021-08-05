ed454ccfc48e9da04779fb7e87692fb986d1381f
Hagen Wierstorf <hwierstorf@audeering.com>
2021-08-05 10:57:11 +0200

    DEBUG

--- tests/test_audiofile.py
+++ tests/test_audiofile.py
@@ -269,8 +269,10 @@ def test_mp3(tmpdir, magnitude, sampling_rate, channels):
 
 
 def test_formats():
-    # script_dir = os.path.dirname(os.path.realpath(__file__))
-    script_dir = 'tests'
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    #script_dir = 'tests'
+    print('DEBUG: ', audeer.list_dir_names(script_dir))
+    print('DEBUG: ', audeer.list_file_names(script_dir))
     assests_dir = os.path.join(script_dir, 'assets')
     files = [
         'gs-16b-1c-44100hz.opus',
