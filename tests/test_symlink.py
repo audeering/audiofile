5a326191527254f0714f1ca763132671bdd3eebf
Hagen Wierstorf <hwierstorf@audeering.com>
2022-03-31 12:26:06 +0200

    Try to use .exe

--- tests/test_symlink.py
+++ tests/test_symlink.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import shutil
 
 import audeer
@@ -8,6 +9,8 @@ def test_symlinks(tmpdir):
     program = 'ffmpeg'
     path = audeer.mkdir(os.path.join(tmpdir, 'bin'))
     command = shutil.which(program)
+    if platform.system() == 'Windows':
+        command += '.exe'
     assert os.path.exists(command)
     local_command = os.path.join(path, program)
     os.symlink(command, local_command)
