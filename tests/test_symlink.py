000b89849a353609bc1c0a2f1155b219038384a9
Johannes Wagner <frankenjoe@hotmail.com>
2022-04-02 20:56:08 +0200

    TST: check for mediainfo in test_symlinks

--- tests/test_symlink.py
+++ tests/test_symlink.py
@@ -2,23 +2,36 @@ import os
 import platform
 import shutil
 
+import pytest
+
 import audeer
 
 
-def test_symlinks(tmpdir):
-    program = 'ffmpeg'
+@pytest.fixture(scope='function')
+def restore_system_path():
+    """Fixture to hide system path in test."""
+    current_path = os.environ['PATH']
+
+    yield
+
+    os.environ['PATH'] = current_path
+
+
+@pytest.mark.parametrize(
+    'program',
+    [
+        'ffmpeg',
+        'mediainfo',
+    ],
+)
+def test_symlinks(tmpdir, restore_system_path, program):
     path = audeer.mkdir(os.path.join(tmpdir, 'bin'))
     command = shutil.which(program)
-    print(command)
     assert os.path.exists(command)
     local_command = os.path.join(path, program)
     if platform.system() == 'Windows':
         local_command += '.EXE'
     os.symlink(command, local_command)
-    # shutil.copyfile(command, local_command)
     assert os.path.exists(local_command)
-    print(local_command)
-    print(os.environ['PATH'])
     os.environ['PATH'] = path
-    print(os.environ['PATH'])
     assert shutil.which(program) == local_command
