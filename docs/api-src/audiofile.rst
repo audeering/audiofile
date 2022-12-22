26740ba36acce10009e5f3c595ae95d609c1896d
Hagen Wierstorf <hwierstorf@audeering.com>
2022-12-22 13:58:34 +0100

    DOC: split up into sub-pages per class/function (#94)

---
+++ docs/api-src/audiofile.rst
@@ -0,0 +1,17 @@
+audiofile
+=========
+
+.. automodule:: audiofile
+
+.. autosummary::
+    :toctree:
+    :nosignatures:
+
+    read
+    write
+    bit_depth
+    channels
+    duration
+    samples
+    sampling_rate
+    convert_to_wav
--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,9 @@
 import configparser
 from datetime import datetime
+import shutil
 import os
-from subprocess import check_output
 
+import audeer
 
 config = configparser.ConfigParser()
 config.read(os.path.join('..', 'setup.cfg'))
@@ -13,13 +14,8 @@ config.read(os.path.join('..', 'setup.cfg'))
 project = config['metadata']['name']
 copyright = f'2018-{datetime.now().year} audEERING GmbH'
 author = config['metadata']['author']
-# The x.y.z version read from tags
-try:
-    version = check_output(['git', 'describe', '--tags', '--always'])
-    version = version.decode().strip()
-except Exception:
-    version = '<unknown>'
-title = '{} Documentation'.format(project)
+version = audeer.git_repo_version()
+title = 'Documentation'
 
 
 # General -----------------------------------------------------------------
@@ -27,7 +23,14 @@ title = '{} Documentation'.format(project)
 master_doc = 'index'
 extensions = []
 source_suffix = '.rst'
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    'build',
+    'tests',
+    'Thumbs.db',
+    '.DS_Store',
+    'api-src',
+]
+templates_path = ['_templates']
 pygments_style = None
 extensions = [
     'jupyter_sphinx',
@@ -52,6 +55,11 @@ autodoc_mock_imports = ['soundfile']
 copybutton_prompt_text = r'>>> |\.\.\. |\$ '
 copybutton_prompt_is_regexp = True
 
+# Disable auto-generation of TOC entries in the API
+# https://github.com/sphinx-doc/sphinx/issues/6316
+toc_object_entries = False
+
+
 # HTML --------------------------------------------------------------------
 
 html_theme = 'sphinx_audeering_theme'
@@ -64,3 +72,15 @@ html_context = {
     'display_github': True,
 }
 html_title = title
+
+
+# Copy API (sub-)module RST files to docs/api/ folder ---------------------
+audeer.rmdir('api')
+audeer.mkdir('api')
+api_src_files = audeer.list_file_names('api-src')
+api_dst_files = [
+    audeer.path('api', os.path.basename(src_file))
+    for src_file in api_src_files
+]
+for src_file, dst_file in zip(api_src_files, api_dst_files):
+    shutil.copyfile(src_file, dst_file)
