81dc696595147666820cf18864b67a49bf73e988
Hagen Wierstorf <hwierstorf@audeering.com>
2026-01-07 12:06:53 +0100

    Specify exact gstreamer package

--- docs/benchmark/install_dependencies.sh
+++ docs/benchmark/install_dependencies.sh
@@ -5,4 +5,4 @@ sudo apt install -y sox ffmpeg libsox-fmt-mp3
 sudo apt install -y libavresample-dev libswresample-dev libavutil-dev
 sudo apt install -y libavcodec-dev libavformat-dev libsamplerate0-dev
 sudo apt install -y libcairo2-dev libmad0-dev libgirepository1.0-dev
-sudo apt install -y gstreamer-1.0
+sudo apt install -y gir1.2-gstreamer-1.0
