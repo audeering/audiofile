4be9ab69562ec199fafbccedc40a011cc517dc2f
Hagen Wierstorf <hwierstorf@audeering.com>
2026-01-07 14:08:54 +0100

    Update docs/conftest.py

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,30 +1,20 @@
 from doctest import ELLIPSIS
 from doctest import NORMALIZE_WHITESPACE
 
-import sybil
+from sybil import Sybil
 from sybil.parsers.rest import DocTestParser
 from sybil.parsers.rest import PythonCodeBlockParser
 
+from audiofile.core.conftest import run_in_tmpdir  # noqa: F401
+
 
 # Collect doctests
-#
-# We use several `sybil.Sybil` instances
-# to pass different fixtures for different files
-#
 parsers = [
     DocTestParser(optionflags=ELLIPSIS + NORMALIZE_WHITESPACE),
     PythonCodeBlockParser(),
 ]
-pytest_collect_file = sybil.sybil.SybilCollection(
-    (
-        sybil.Sybil(
-            parsers=parsers,
-            filenames=[
-                "usage.rst",
-            ],
-            fixtures=[
-                "run_in_tmpdir",
-            ],
-        ),
-    )
+pytest_collect_file = Sybil(
+    parsers=parsers,
+    filenames=["usage.rst"],
+    fixtures=["run_in_tmpdir"],
 ).pytest()
