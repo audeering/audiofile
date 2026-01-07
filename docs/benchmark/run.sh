22050e7e9c52c77f19bc3bc3fa34fa7d8ffb4717
Hagen Wierstorf <hwierstorf@audeering.com>
2026-01-07 12:16:59 +0100

    Define Python version at single place

--- docs/benchmark/run.sh
+++ docs/benchmark/run.sh
@@ -1,3 +1,5 @@
+PYTHON_VERSION="3.12"
+
 ## declare an array variable
 declare -a exts=("wav" "ogg" "flac" "mp3" "mp4")
 
@@ -11,8 +13,8 @@ fi
 ## now loop through the above array
 for i in "${exts[@]}"
 do
-    uv run --python 3.12 benchmark_read.py --ext "$i"
-    uv run --python 3.12 benchmark_info.py --ext "$i"
+    uv run --python $PYTHON_VERSION benchmark_read.py --ext "$i"
+    uv run --python $PYTHON_VERSION benchmark_info.py --ext "$i"
 done
 
-uv run --python 3.12 plot.py
+uv run --python $PYTHON_VERSION plot.py
